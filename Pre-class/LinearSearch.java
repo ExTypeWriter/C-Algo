61137b169790b1a92e2951014ea101dfa03dba23
Pichawat A <pichawat43695@gmail.com>
2023-08-22 20:08:10 +0700

    Pre-class Linear search

--- Pre-class/LinearSearch.java
+++ Pre-class/LinearSearch.java
@@ -1,8 +1,14 @@
 class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = { 42, 18, 76, 29, 55, 63, 91, 14, 37, 68 };
+        int[] arr = { 42, 18, 17, 29, 55, 63, 91, 14, 37, 68 };
         int out = linearSearch(arr, 1);
         System.out.println(out);
+        if(UniqueElement(arr)){
+            System.out.println("Unique Array.");
+        }
+        else{
+            System.out.println("Not Unique Array.");
+        }
     }
 
     private static int linearSearch(int[] arr, int n) {
@@ -16,10 +22,16 @@ class LinearSearch {
             }
             i++;
         }
-        if(arr[i] - n != 0) {
-            System.out.println("Target not found! return the first index element.");
-        }
-
         return arr[location];
+    }
+    private static boolean UniqueElement(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
