3341667fc97fc469775c16f0bfe5c1a46421176f
Pichawat <pichawat43695@gmail.com>
2023-09-20 08:25:58 +0700

    Committing local changes before pulling

--- Pre-class/LinearSearch.java
+++ Pre-class/LinearSearch.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 class LinearSearch {
     public static void main(String[] args) {
         int[] arr = { 42, 18, 17, 29, 55, 63, 91, 14, 37, 68 };
-        int out = linearSearch(arr, 1);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input target: ");
+        int search_target = sc.nextInt();
+        int out = linearSearch(arr, search_target);
         System.out.println(out);
         if(UniqueElement(arr)){
             System.out.println("Unique Array.");
@@ -14,15 +18,24 @@ class LinearSearch {
     private static int linearSearch(int[] arr, int n) {
         int location = 0;
         int i = 0;
+        boolean found = false;
         while (i < arr.length) {
             if (arr[i] - n == 0) {
-                System.out.println("Target found!");
                 location = i;
+                found = true;
                 break;
             }
             i++;
         }
-        return arr[location];
+        if(found == true)
+        {
+          System.out.println("Target found!");
+          return arr[location];
+        }
+        else{
+            System.out.println("Not found");
+            return -999999;
+        }
     }
     private static boolean UniqueElement(int[] arr){
         for(int i=0;i<arr.length;i++){
